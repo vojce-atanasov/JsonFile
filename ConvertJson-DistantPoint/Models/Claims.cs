cb3c100323761c80d8d3e100c2cd8ae01011927e
vojce-atanasov <76123040+vojce-atanasov@users.noreply.github.com>
2022-02-07 00:45:40 +0100

    Json

---
+++ ConvertJson-DistantPoint/Models/Claims.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvertJson_DistantPoint.Models
+{
+    public class Claims
+    {
+        [JsonProperty("Claim Type")]
+        public string ClaimType { get; set; }
+
+        [JsonProperty("Value")]
+        public string Value { get; set; }
+    }
+}
---
+++ ConvertJson-DistantPoint/Models/Jsonfile.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvertJson_DistantPoint.Models
+{
+    public class Jsonfile
+    {
+        public IFormFile FileJ { get; set; }
+
+        public string Content { get; set; }
+    }
+}
